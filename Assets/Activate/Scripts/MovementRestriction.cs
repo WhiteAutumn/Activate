c36a4b37ea1043f8c7bb334e1e4c2d0ee2aa59c3
White Autumn <white@autumn.solutions>
2019-07-10 22:52:17 +0200

    Added MovementRestriction script

---
+++ Assets/Activate/Scripts/MovementRestriction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovementRestriction : MonoBehaviour
+{
+    Transform ourTransform;
+    Rigidbody ourRigidbody;
+    
+    void Start()
+    {
+        ourTransform = gameObject.transform;
+        ourRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (ourTransform.localPosition.y > 0)
+        {
+            ourRigidbody.velocity = Vector3.zero;
+            ourTransform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            ourTransform.localPosition = new Vector3(0, ourTransform.localPosition.y, 0);
+        }
+    }
+}
