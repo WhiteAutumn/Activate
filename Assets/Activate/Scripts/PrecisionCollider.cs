617925729b69cfc072d90ffe14f1e9ac0bfa091d
White Autumn <white@autumn.solutions>
2019-07-15 23:21:26 +0200

    Continued work on precision collision

--- Assets/Activate/Scripts/PrecisionCollider.cs
+++ Assets/Activate/Scripts/PrecisionCollider.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PrecisionCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color(156, 255, 175);
+	}
 }
