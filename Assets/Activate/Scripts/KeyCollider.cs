b79f2c1a63c7c06832ba639292d952017461bc18
White Autumn <white@autumn.solutions>
2019-07-19 12:56:27 +0200

    Completed key collision system

---
+++ Assets/Activate/Scripts/KeyCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class KeyCollider : MonoBehaviour
+{
+	public float Offset;
+
+	void OnDrawGizmosSelected()
+	{
+		Transform ourTransform = transform;
+		Vector3 transformedOffset = ourTransform.position + ourTransform.TransformVector(new Vector3(0, Offset, 0));
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transformedOffset + ourTransform.TransformVector(Vector3.forward),transformedOffset + ourTransform.TransformVector(Vector3.back));
+		Gizmos.DrawLine(transformedOffset + ourTransform.TransformVector(Vector3.right),transformedOffset + ourTransform.TransformVector(Vector3.left));
+	}
+}
---
+++ Assets/Activate/Scripts/KeyCollisionValidator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class KeyCollisionValidator : MonoBehaviour
+{
+    Transform ourTransform;
+    SphereCollider ourCollider;
+    Vector3 previousPos;
+    Vector3 colliderOffset;
+    readonly Vector3[] colliderEdgeOffsets = new Vector3[6];
+    float colliderRadius;
+    float minimumVelocity;
+    
+    void Start()
+    {
+        ourTransform = transform;
+        previousPos = ourTransform.position;
+        ourCollider = GetComponent<SphereCollider>();
+        
+        colliderOffset = ourCollider.center;
+        float radius = ourCollider.radius;
+
+        Transform currentTransform = ourTransform;
+        while (true)
+        {
+            Vector3 localScale = currentTransform.localScale;
+            radius *= Mathf.Max(localScale.x, localScale.y, localScale.z);
+
+            if (currentTransform.parent == null) 
+                break;
+            
+            currentTransform = currentTransform.parent;
+        }
+        
+        colliderEdgeOffsets[0] =  new Vector3(radius, 0, 0);
+        colliderEdgeOffsets[1] =  new Vector3(-radius, 0, 0);
+        colliderEdgeOffsets[2] =  new Vector3(0, radius, 0);
+        colliderEdgeOffsets[3] =  new Vector3(0, -radius, 0);
+        colliderEdgeOffsets[4] =  new Vector3(0, 0, radius);
+        colliderEdgeOffsets[5] =  new Vector3(0, 0, -radius);
+        
+        colliderRadius = radius;
+        minimumVelocity = radius / 2;
+    }
+    
+    void Update()
+    {
+        float currentVelocity = Vector3.Distance(ourTransform.position, previousPos);
+        
+        if (currentVelocity > minimumVelocity)
+        {
+            Vector3 center = ourTransform.position + colliderOffset;
+            Vector3 previousCenter = previousPos + colliderOffset;
+
+            foreach (Vector3 edgeOffset in colliderEdgeOffsets)
+            {
+                Vector3 edge = center + edgeOffset;
+                Vector3 previousEdge = previousCenter + edgeOffset;
+                
+                bool hitKeycap = false;
+                RaycastHit[] hits = Physics.RaycastAll(previousEdge, edge - previousEdge, Vector3.Distance(edge, previousEdge));
+                foreach (RaycastHit hit in hits)
+                {
+                    if (hit.collider.CompareTag("keycap"))
+                    {
+                        KeyCollider keyCollider = hit.collider.gameObject.GetComponent<KeyCollider>();
+                        Transform keyTransform = keyCollider.transform;
+
+                        keyTransform.position =
+                            ourTransform.position +
+                            keyTransform.TransformDirection(new Vector3(0, -colliderRadius)) +
+                            keyTransform.TransformVector(new Vector3(0, -keyCollider.Offset, 0));
+                        keyTransform.localPosition = new Vector3(0, keyTransform.localPosition.y, 0);
+
+                        hitKeycap = true;
+                        break;
+                    }
+                }
+
+                if (hitKeycap)
+                    break;
+            }
+        }
+
+        previousPos = transform.position;
+    }
+}
