580b0addf5a83f66bdc2ab861568696b9c197f63
White Autumn <white@autumn.solutions>
2019-07-22 21:37:52 +0200

    Started working on haptic feedback

---
+++ Assets/Demo/Scripts/HapticKeyFeedback.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HapticKeyFeedback : MonoBehaviour, IPassiveKeyListener, IActiveKeyListener
+{
+	public void OnKeyDown(string signal)
+	{
+		
+	}
+
+	public void OnKeyUpdate(string signal, float value)
+	{
+		
+	}
+}
